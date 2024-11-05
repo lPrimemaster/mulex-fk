48bf9815c5b7343471e849d2716ed1946d17d899
César Godinho <cesarsky5@gmail.com>
2024-11-05 22:32:35 +0000

    Add rdb reader helper binary.

---
+++ ctargets/rdbreader.cpp
@@ -0,0 +1,49 @@
+#include "../mxrdb.h"
+
+int main(int argc, char* argv[])
+{
+	if(argc != 4)
+	{
+		std::cout << "USAGE: ./rdbreader <hostname> <key> <type>" << std::endl;
+		return 0;
+	}
+
+	if(!mulex::SysConnectToExperiment(argv[1]))
+	{
+		return 0;
+	}
+
+	mulex::RdbAccess ra;
+
+	std::string key = argv[2];
+	std::string type = argv[3];
+
+	std::cout << "===== Value =====" << std::endl;
+	if(type == "string")
+	{
+		mulex::mxstring<512> fa = ra[key];
+		std::cout << fa.c_str() << std::endl;
+	}
+	else if(type == "int")
+	{
+		std::int32_t fa = ra[key];
+		std::cout << fa << std::endl;
+	}
+	else if(type == "float")
+	{
+		float fa = ra[key];
+		std::cout << fa << std::endl;
+	}
+	else if(type == "double")
+	{
+		double fa = ra[key];
+		std::cout << fa << std::endl;
+	}
+	else
+	{
+		mulex::LogError("Type <%s> not implemented on rdbreader or overall unrecognized.", type.c_str());
+	}
+	std::cout << "=================" << std::endl;
+	
+	mulex::SysDisconnectFromExperiment();
+}
