fdefaadc1c6ba5c6a9e2becf8cdbebb90bfce6c8
César Godinho <cesarsky5@gmail.com>
2024-11-05 22:34:33 +0000

    Add daemonization on linux. Test argparse.

--- test/argparse.cpp
+++ test/argparse.cpp
@@ -1,28 +1,38 @@
 #include "test.h"
 #include "../mxsystem.h"
+#include <sstream>
 
 int main(int argc, char* argv[])
 {
 	using namespace mulex;
-
-	SysAddArgument("name", 'n', true, [](const std::string& val){
-		std::cout << "Got arg: " << val << std::endl;
-	});
-
-	SysAddArgument("daemon", 'D', false, [](const std::string& val){
-		std::cout << "Running daemon." << std::endl;
-	});
 
 	// Fails
 	SysAddArgument("help", 'h', false, [](const std::string& val){
 		std::cout << "Fake help." << std::endl;
 	});
 
+#ifdef __linux__
+	ASSERT_THROW(SysGetBinaryName() == "test_argparse");
+#else
+	ASSERT_THROW(SysGetBinaryName() == "test_argparse.exe");
+#endif
 
-	SysAddArgument("proc", 'p', false, [](const std::string& val){
-		std::cout << SysGetBinaryName() << std::endl;
-	});
+	std::stringstream ss;
+	{
+		cout_redirect redg(ss.rdbuf());
+		
+		char* argvf[] = {
+			(char*)"",
+			(char*)"-h",
+			NULL
+		};
 
-	SysParseArguments(argc, argv);
+		SysParseArguments(2, argvf);
+
+		ASSERT_THROW(ss.str() == 
+			"Arguments help:\n\t-D\t--daemon\n\t\tTurn the current process into a daemon (linux only).\n\n\t-h\t--help\n\t\tPrints this help message.\n\n"
+		);
+	}
+	std::cout << ss.str() << std::endl;
 	return 0;
 }
