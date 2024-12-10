5f74fd760c63fa827104698a7d5c653d6f071228
César Godinho <cesarsky5@gmail.com>
2024-12-10 00:49:32 +0000

    Add run boilerplate code.

---
+++ mxrun.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "mxtypes.h"
+#include "mxlogger.h"
+
+namespace mulex
+{
+	enum class RunStatus : std::uint8_t
+	{
+		STOPPED,
+		RUNNING,
+		STARTING,
+		STOPPING
+	};
+
+	void RunInitVariables();
+	MX_RPC_METHOD bool RunStart();
+	MX_RPC_METHOD void RunStop();
+} // namespace mulex
---
+++ run/mxrun.cpp
@@ -0,0 +1,86 @@
+#include "../mxrun.h"
+#include "../mxrdb.h"
+#include "../mxevt.h"
+
+namespace mulex
+{
+	static const char* RunStatusToString(RunStatus status)
+	{
+		switch(status)
+		{
+			case RunStatus::STOPPED:  return "STOPPED";
+			case RunStatus::RUNNING:  return "RUNNING";
+			case RunStatus::STARTING: return "STARTING";
+			case RunStatus::STOPPING: return "STOPPING";
+		}
+		return "";
+	}
+
+	void RunInitVariables()
+	{
+		LogTrace("[mxrun] Initializing run metadata.");
+
+		// Status MEMO:
+		// 0 - Stopped
+		// 1 - Running
+		// 2 - Starting
+		// 3 - Stopping
+		RdbNewEntry("/system/run/status", RdbValueType::UINT8, 0);
+
+		// On server init run status is always set to stopped regardless of the saved value
+		RdbWriteValueDirect("/system/run/status", std::uint8_t(0));
+		
+		// Run number incrementing indefinitely 
+		RdbNewEntry("/system/run/number", RdbValueType::UINT64, 0);
+
+		// Run last start timestamp
+		RdbNewEntry("/system/run/timestamp", RdbValueType::INT64, 0);
+	}
+
+	bool RunStart()
+	{
+		// Check the status
+		RunStatus status = static_cast<RunStatus>(RdbReadValueDirect("/system/run/status").asType<std::uint8_t>());
+
+		if(status != RunStatus::STOPPED)
+		{
+			LogError("[mxrun] Failed to start run. Status is '%s'. Status 'STOPPED' is required.", RunStatusToString(status));
+			return false;
+		}
+
+		RdbWriteValueDirect("/system/run/status", std::uint8_t(2));
+
+		// Increment the run
+		std::uint64_t runno = RdbReadValueDirect("/system/run/number").asType<std::uint64_t>() + 1;
+		RdbWriteValueDirect("/system/run/number", runno);
+
+		// For now we don't do anything else
+		
+		RdbWriteValueDirect("/system/run/timestamp", SysGetCurrentTime());
+		RdbWriteValueDirect("/system/run/status", std::uint8_t(1));
+
+		LogTrace("[mxrun] RunStart() OK.");
+
+		return true;
+	}
+
+	void RunStop()
+	{
+		// Check the status
+		RunStatus status = static_cast<RunStatus>(RdbReadValueDirect("/system/run/status").asType<std::uint8_t>());
+
+		if(status != RunStatus::RUNNING)
+		{
+			LogError("[mxrun] Failed to stop run. Status is '%s'. Status 'RUNNING' is required.", RunStatusToString(status));
+			return;
+		}
+
+		RdbWriteValueDirect("/system/run/status", std::uint8_t(3));
+
+		// For now we don't do anything else
+		
+		RdbWriteValueDirect("/system/run/status", std::uint8_t(0));
+
+		LogTrace("[mxrun] RunStop() OK.");
+	}
+}
