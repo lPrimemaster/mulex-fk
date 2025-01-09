f4588d3fb87f28fabf4287a323d6fa4eef4a5188
César Godinho <cesarsky5@gmail.com>
2025-01-09 15:28:15 +0000

    Add backend library export config for `find_package` support.

--- cmake/InstallMulex.cmake
+++ cmake/InstallMulex.cmake
@@ -1,11 +1,36 @@
 macro(install_mulex)
-	# Install server
+	# Install server binary
 	install(TARGETS mxmain DESTINATION bin)
 
-	# Install backend libs
-	install(TARGETS mxrpc mxhttp mxrdb mxdrv mxbck mxrun DESTINATION lib)
+	# Common includes
+	install(FILES network/socket.h DESTINATION include/network)
+	install(FILES network/rpc.h DESTINATION include/network)
+endmacro()
 
-	# Backend includes
-	file(GLOB headers ${CMAKE_SOURCE_DIR}/*.h)
-	install(FILES ${headers} DESTINATION include)
+macro(install_target target header)
+	include(CMakePackageConfigHelpers)
+
+	install(TARGETS ${target}
+		EXPORT ${target}Targets
+		ARCHIVE DESTINATION lib
+		INCLUDES DESTINATION include
+	)
+
+	if(NOT ${header} STREQUAL NONE)
+		install(FILES ${CMAKE_SOURCE_DIR}/${header} DESTINATION include)
+	endif()
+
+	set(LIB_TARGET ${target})
+
+	configure_package_config_file(
+		${CMAKE_SOURCE_DIR}/LibConfig.cmake.in
+		${CMAKE_CURRENT_BINARY_DIR}/${target}Config.cmake
+		INSTALL_DESTINATION lib/cmake/${target}
+	)
+
+	configure_file(${CMAKE_SOURCE_DIR}/LibConfigVersion.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/${target}ConfigVersion.cmake)
+
+	install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${target}Config.cmake DESTINATION lib/cmake/${target})
+	install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${target}ConfigVersion.cmake DESTINATION lib/cmake/${target})
+	install(EXPORT ${target}Targets FILE ${target}Targets.cmake NAMESPACE Mx:: DESTINATION lib/cmake/${target})
 endmacro()
