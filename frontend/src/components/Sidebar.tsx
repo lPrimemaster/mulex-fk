3a0ecb03c68c37bf6ef7f7daa37ca497d6f0cabe
César Godinho <cesarsky5@gmail.com>
2025-04-01 13:37:04 +0100

    Add android style developer mode activation.

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -12,9 +12,30 @@ import {
 
 import metadata from '../lib/metadata';
 
+const [devTools, setDevTools] = createSignal(false);
+
 const Sidebar: Component = () => {
 
 	const [showAbout, setShowAbout] = createSignal(false);
+	const [showDevPopup, setShowDevPopup] = createSignal(false);
+	const [devSteps, setDevSteps] = createSignal(9);
+
+	let timeout: NodeJS.Timeout;
+
+	function checkDev() {
+		setDevSteps(devSteps() - 1);
+
+		if(devSteps() < 1) {
+			setDevTools(true);
+			return;
+		}
+
+		if(devSteps() < 8) {
+			setShowDevPopup(true);
+			clearTimeout(timeout);
+			timeout = setTimeout(() => { setShowDevPopup(false); setDevSteps(9); }, 2000);
+		}
+	}
 
 	return (
 		<div class="m-0 border-r-2 hover:shadow-2xl shadow-lg border-solid border-black-200 min-h-full min-w-36 max-w-36" style="width:10%;position:fixed!important;z-index:1;overflow:auto;">
@@ -51,6 +72,13 @@ const Sidebar: Component = () => {
 								<A href='/history'>History</A>
 							</NavigationMenuTrigger>
 						</NavigationMenuItem>
+						<Show when={devTools()}>
+							<NavigationMenuItem>
+								<NavigationMenuTrigger>
+									<A href='/debug'>Debug</A>
+								</NavigationMenuTrigger>
+							</NavigationMenuItem>
+						</Show>
 						<NavigationMenuItem>
 							<NavigationMenuTrigger onClick={() => setShowAbout(true)}>
 								About
@@ -58,10 +86,15 @@ const Sidebar: Component = () => {
 						</NavigationMenuItem>
 					</NavigationMenu>
 				</div>
+				<Show when={showDevPopup()}>
+					<div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 p-5 rounded-md shadow-lg">
+						{ devTools() ? devSteps() > 1 ? "You are already a developer." : "You are now a developer." : `You are ${devSteps()} steps away from entering the developer mode.`}
+					</div>
+				</Show>
 				<footer class="p-5" style="position:fixed; bottom:0">
 					<div class="grid justify-items-center">
 						<small>Version {__APP_VNAME__}</small>
-						<small class="text-gray-400">v{__APP_VERSION__}</small>
+						<small class="text-gray-400 cursor-default select-none" onClick={checkDev}>v{__APP_VERSION__}</small>
 					</div>
 				</footer>
 			</div>
