4b1fe2f8890cd7dd6e74546a56d4c3b3d57c9bb0
lPrimemaster <cesarsky5@gmail.com>
2025-11-18 10:01:02 +0000

    Testing kpm.

--- install/on_install.py
+++ install/on_install.py
@@ -7,7 +7,7 @@ import json
 
 
 # Write the package additional files for kpm to read at install time
-def write_json(cpath, install_loc, package_name):
+def generate_output(cpath, install_loc, package_name):
     output = {}
     output['additional_files'] = []
     for dp, dn, files in os.walk(install_loc):
@@ -15,20 +15,15 @@ def write_json(cpath, install_loc, package_name):
             output['additional_files'].append(
                 str(Path(os.path.join(dp, file)).resolve())
             )
-
-    with open(f'{cpath}/{package_name}.user.json', 'w') as f:
-        json.dump(output, f)
+    return output
 
 
-if __name__ == '__main__':
-    metadata = json.load(sys.stdin)
-
-    package_name = metadata['package_name']
-    cache_path = Path(metadata['cache_path']).resolve()
-    cwd = Path(metadata['install_prefix']).resolve()
+def post_install(package_name, cache_path, install_prefix):
+    cache_path = Path(cache_path).resolve()
+    cwd = Path(install_prefix).resolve()
     install_loc = (Path(site.getusersitepackages()) / 'pymx').resolve()
     install_loc.mkdir(parents=True, exist_ok=True)
 
     shutil.copytree(cwd / 'pymx', install_loc, dirs_exist_ok=True)
 
-    write_json(cache_path, install_loc, package_name)
+    return generate_output(cache_path, install_loc, package_name)
